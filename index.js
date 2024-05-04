2b0f33fceb3fc9683794e62d5a5ceee2f63ee6b0
Dev <dev@git.com>
2024-05-04 23:24:46 +0530

    Registration MongoDB connectivity completed. Axios username availability check pending

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {dirname} from "path";
 import { fileURLToPath } from "url";
-
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const HOST="127.0.0.1";
@@ -9,19 +11,76 @@ const PORT = process.env.PORT || 3000;
 const APP = express();
 
 APP.use(express.static("public"));
+APP.use(bodyParser.urlencoded({extended: true}));
+
+// Connect to MongoDB
+
+mongoose.connect('mongodb://localhost:27017/LLP_APP');
+
+const testSchema = new mongoose.Schema(
+                                        {
+                                            username: String,
+                                            email: String,
+                                            plan: String,
+                                            password: String
+                                        }
+);
+
+const testUser = mongoose.model('userTest',testSchema);
 
 // ROUTES
 
-// '/' -> Homepage
+// '/' -> Homepage endpoint
+
 APP.get("/",(req, res)=> {
                                     //res.send("<h1>THE APP IS GOING LIVE</h1>");
                                     res.sendFile(__dirname+"/public/home.html");
                                 }
 );
 
-// '/register' -> Registration
-APP.post("/register",(req,res) => {
-        res.send("Welcome to the User Dashboard");
+// '/register' -> Registration endpoint
+
+APP.post("/register", async (req,res) => {
+        const { username, email, plan, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 12);
+        try 
+        {
+            const tuser = new testUser( {username, email, plan, password: hashedPassword});
+            await tuser.save();
+            res.send("Registration successful");
+        }
+        catch( error )
+        {
+            console.log(error);
+            res.status(500).send("registration failed");
+        }
+    }
+);
+
+// '/check-username -> Checking username availability endpoint
+
+APP.get('/check-username', async (req, res) =>
+    {
+        const { username } = req.query;
+        try
+        {
+            console.log("Here 1");
+            const userAny = await testUser.findOne({ username });
+            console.log("Here 2");
+            if(userAny)
+            {
+                res.json( { available: false }); //Username already exists
+            }
+            else
+            {
+                res.json( { available: true }); //Username is available
+            }
+        }
+        catch(error)
+        {
+            console.error("Error checking username availability",error);
+            res.status(500).json( {error: "Internal Server Error" });
+        }
     }
 );
 
