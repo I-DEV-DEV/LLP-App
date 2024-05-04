04bd58e449dd281adb0694eb23ab34228e1f4858
Dev <dev@git.com>
2024-05-04 18:23:28 +0530

    Registration processes initial stage completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import {dirname} from "path";
 import { fileURLToPath } from "url";
 
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const HOST="127.0.0.1";
 const PORT = process.env.PORT || 3000;
@@ -11,12 +12,19 @@ APP.use(express.static("public"));
 
 // ROUTES
 
-// '/'
+// '/' -> Homepage
 APP.get("/",(req, res)=> {
                                     //res.send("<h1>THE APP IS GOING LIVE</h1>");
                                     res.sendFile(__dirname+"/public/home.html");
                                 }
 );
+
+// '/register' -> Registration
+APP.post("/register",(req,res) => {
+        res.send("Welcome to the User Dashboard");
+    }
+);
+
 
 // Server listening
 APP.listen(PORT,HOST,()=> {
